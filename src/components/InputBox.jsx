25d18f37757eb925e567024159118e68a7c7cf38
ayoncoding404 <muhayon.rxz@gmail.com>
2025-01-30 03:23:21 +0600

    add custom class

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -6,7 +6,7 @@ const InputBox = ({
   className = "",
 }) => {
   return (
-    <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
         <label className="text-black/40 mb-2 inline-block">label</label>
         <input
