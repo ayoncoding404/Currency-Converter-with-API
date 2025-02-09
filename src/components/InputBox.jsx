2f3659feb4d4854e3cdc7ba4413b5fd47e81d08e
ayoncoding404 <muhayon.rxz@gmail.com>
2025-02-10 01:06:20 +0600

    add input  ID

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -14,8 +14,9 @@ const InputBox = ({
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label htmlFor="myAmount" className="text-black/40 mb-2 inline-block">{label}</label>
         <input
+          id="myAmount"
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
