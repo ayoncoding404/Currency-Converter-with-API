a677d1e9ededdc2333c09217f83171753bd6df98
ayoncoding404 <muhayon.rxz@gmail.com>
2025-02-10 02:04:00 +0600

    pass value in function

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import { InputBox } from './components'
-import useCurrencyInfo from './hooks/useCurrencyInfo'
+import { useState } from "react";
+import "./App.css";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
-  
- const[amount, setAmount] = useState(0);
- const[from, setFrom] = useState("usd")
- const[to, setTo] = useState ("inr")
- const [convertedAmount, setConvertedAmount] =useState(0);
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
 
-  
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
   return (
     <>
-     <h1 className='text-3xl bg-orange-500'>Currency Changing App</h1>
+      <h1 className="text-3xl bg-orange-500">Currency Changing App</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
